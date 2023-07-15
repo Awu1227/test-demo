5cc411161b5ead5a98336dc567c8d5f932eb9cf4
orange <2497436726@qq.com>
2023-07-15 19:54:15 +0800

    部署

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        adsorb: resolve(__dirname, 'adsorb/index.html'),
-        cssExercise: resolve(__dirname, 'cssExercise/index.html'),
-      },
-    },
+    // rollupOptions: {
+    //   input: {
+    //     main: resolve(__dirname, 'index.html'),
+    //     adsorb: resolve(__dirname, 'adsorb/index.html'),
+    //     cssExercise: resolve(__dirname, 'cssExercise/index.html'),
+    //   },
+    // },
+    outDir: 'docs'
   },
 })
