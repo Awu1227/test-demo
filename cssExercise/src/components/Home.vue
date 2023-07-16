cde956d456bd52de71b5a6e50868725fc60af5db
Orange <72426886+Awu1227@users.noreply.github.com>
2023-07-16 19:27:44 +0800

    :bento:静态资源路径修改

--- cssExercise/src/components/Home.vue
+++ cssExercise/src/components/Home.vue
@@ -2,7 +2,7 @@
 	<header class="relative w-full py-4 border border-gray-400  bg-gradient-to-r from-violet-500 to-fuchsia-500 flex justify-center text-2xl text-white font-bold">
 		每日CSS 练习
 		<a href="https://github.com/Awu1227/test-demo/tree/master/cssExercise">
-			<img src="../../public/github.svg" class="absolute right-4" alt="github logo" >
+			<img src="/github.svg" class="absolute right-4" alt="github logo" >
 		</a>
 	</header>
 	<main class=" w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 overflow-hidden">
