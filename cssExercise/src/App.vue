9538651dca9279f0dd2eb8ac971bbec747385a4c
orange <2497436726@qq.com>
2023-07-16 09:30:23 +0800

    fix: 添加vue

---
+++ cssExercise/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	vue sfc
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
+  plugins:[vue()],
   base: '/test-demo/',
   build: {
     rollupOptions: {
