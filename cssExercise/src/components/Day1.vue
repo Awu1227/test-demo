21f9e3aa1e053a69b1576ba890f8f5a342af192d
orange <2497436726@qq.com>
2023-07-16 19:13:15 +0800

    :lipstick:Day1 finished

--- cssExercise/src/components/Day1.vue
+++ cssExercise/src/components/Day1.vue
@@ -1,13 +1,69 @@
 <template>
-	<div>
-		Day1
+	<header class="flex justify-center bg-slate-200 m-4 font-bold text-3xl p-4 rounded-lg">Expanding Cards</header>
+	<div class="container flex">
+		<template v-for=" item of list" :key="item.id"  >
+			<div  class="panel" :class="[item.isActive ? 'active': '']" :style="{'background-image': `url(${item.url})`}" @click="beActive(item.id)">
+				<h3>{{ item.name }}</h3>
+			</div>
+		</template>
+
 	</div>
 </template>
 
 <script setup>
+import {reactive} from 'vue'
+import { panelList } from './data'
+const list = reactive(panelList)
 
+
+const beActive = (id) => {
+	list.forEach(item => item.isActive = false)
+	const currentPanel = list.find(item => item.id === id)
+	currentPanel.isActive = true
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.panel {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 80vh;
+  border-radius: 50px;
+  color: #fff;
+  cursor: pointer;
+  flex: 0.5;
+  margin: 10px;
+  position: relative;
+  -webkit-transition: all 700ms ease-in;
+}
 
+.panel h3 {
+  font-size: 24px;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  margin: 0;
+  opacity: 0;
+}
+
+.panel.active {
+  flex: 5;
+}
+
+.panel.active h3 {
+  opacity: 1;
+  transition: opacity 0.3s ease-in 0.4s;
+}
+
+@media (max-width: 480px) {
+  .container {
+    width: 100vw;
+  }
+
+  .panel:nth-of-type(4),
+  .panel:nth-of-type(5) {
+    display: none;
+  }
+}
 </style>
---
+++ cssExercise/src/components/data/index.ts
@@ -0,0 +1,34 @@
+export const panelList = [
+	{
+		id: 1,
+		name: 'Explore The World',
+		url: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+		isActive: true
+	},
+	{
+		id: 2,
+		name: 'Wild Frost',
+		url: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+		isActive: false
+	},
+	{
+		id: 3,
+		name: 'Sunny Beach',
+		url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+		isActive: false
+		
+	},
+	{
+		id: 4,
+		name: 'City On Winter',
+		url: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+		isActive: false
+		
+	},
+	{
+		id: 5,
+		name: 'Mountains - Clouds',
+		url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+		isActive: false
+	}
+]
