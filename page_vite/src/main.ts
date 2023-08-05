b353eaf3f9eeba2858a87baafb05cefadd34c167
orange <2497436726@qq.com>
2023-08-06 06:45:55 +0800

    :sparkles:添加随机精灵

--- page_vite/src/main.ts
+++ page_vite/src/main.ts
@@ -1,6 +1,9 @@
 import CreateLayout from "./layout";
 import CreateCoordinate from "./coordinate";
+import createRandomSprite from "./randomSprite";
 
 const ly = new CreateLayout();
 
 const coordinate = new CreateCoordinate(ly);
+
+const sprites = new createRandomSprite(ly);
--- page_vite/src/randomSprite/index.ts
+++ page_vite/src/randomSprite/index.ts
@@ -1,3 +1,50 @@
-export class createRandomSprite {
-  constructor() {}
+import Konva from "konva";
+import CreateLayout from "../layout";
+export default class createRandomSprite {
+  spriteGroup = new Konva.Group({
+    name: "ranDomSprite",
+  });
+  constructor(ly: CreateLayout) {
+    this.createShape(300);
+
+    ly.layer.add(this.spriteGroup);
+  }
+  /**@description 生成Shape */
+  createShape(count: number) {
+    for (let i = 0; i < count; i++) {
+      const polygon = new Konva.RegularPolygon({
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight,
+        sides: this.getRandomSize(),
+        radius: 6,
+        fill: this.getRandomColor(),
+      });
+      this.spriteGroup.add(polygon);
+    }
+  }
+  /**@description 获取随机多边形 */
+  getRandomSize() {
+    const random = Math.random() * 10;
+    if (random < 3) {
+      return 3;
+    } else if (random < 6) {
+      return 4;
+    } else {
+      return 5;
+    }
+  }
+
+  /**@description 获取随机颜色 */
+  getRandomColor() {
+    const random = Math.random() * 10;
+    if (random < 3) {
+      return "purple";
+    } else if (random < 6) {
+      return "lightyellow";
+    } else if (random < 8) {
+      return "lightblue";
+    } else {
+      return "pink";
+    }
+  }
 }
