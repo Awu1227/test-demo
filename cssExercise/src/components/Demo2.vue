6917c5c1726563d57692d50f56600fd2c6421efb
orange <2497436726@qq.com>
2023-11-06 22:35:11 +0800

    fix: rotateX

--- cssExercise/src/components/Demo2.vue
+++ cssExercise/src/components/Demo2.vue
@@ -1,38 +1,55 @@
 <template>
-	<header class="flex justify-center bg-slate-200 m-4 font-bold text-3xl p-4 rounded-lg">CSS3D Experience</header>
-	<div class="container">
-    <div class="title">Feed your eyes, feed your soul.</div>
-    <div class="sub_title">开眼，看更好的世界。</div>
-	</div>
+  <header class="flex justify-center bg-slate-200 m-4 font-bold text-3xl p-4 rounded-lg absolute top">CSS3D Experience</header>
+  <div class="my_container" ref="container">
+    <div class="title" :style="rotate3DObject">Feed your eyes, feed your soul.</div>
+    <div class="sub_title" :style="rotate3DObject">开眼，看更好的世界。</div>
+  </div>
 </template>
 
 <script setup>
-import {reactive} from 'vue'
-import { panelList } from './data'
-const list = reactive(panelList)
+import { onMounted, reactive, ref, watch, watchEffect } from 'vue'
+import { useMouse } from '@vueuse/core'
 
+const { x, y, sourceType } = useMouse()
+const deltaRotateX = ref(0)
+const deltaRotateY = ref(0)
+const rotate3DObject = ref({})
+const container = ref(null)
 
-const beActive = (id) => {
-	list.forEach(item => item.isActive = false)
-	const currentPanel = list.find(item => item.id === id)
-	currentPanel.isActive = true
-}
+const width = window.innerWidth
+const height = window.innerHeight
+
+watch(x, () => {
+  deltaRotateY.value = (width / 2 - x.value) * 0.1
+  rotate3DObject.value = {
+    transform: `rotateX(${deltaRotateX.value}deg) rotateY(${deltaRotateY.value}deg)`
+  }
+})
+
+watch(y, () => {
+  deltaRotateX.value = (y.value - height / 2) * 0.1
+  rotate3DObject.value = {
+    transform: `rotateX(${deltaRotateX.value}deg) rotateY(${deltaRotateY.value}deg)`
+  }
+})
 </script>
 
 <style scoped>
-
+.my_container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 .title {
   font-size: 70px;
   font-weight: large;
 }
-@media (max-width: 480px) {
-  .container {
-    width: 100vw;
-  }
-
-  .panel:nth-of-type(4),
-  .panel:nth-of-type(5) {
-    display: none;
-  }
+.title,
+.sub_title {
+  text-align: center;
+  cursor: default;
 }
 </style>
