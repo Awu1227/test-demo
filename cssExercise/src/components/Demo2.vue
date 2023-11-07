477bbe5056096848e17dfc97f746ff4afbb2ae6e
xuewuding <2497436726@qq.com>
2023-11-07 16:33:22 +0800

    fix: rotate3D

--- cssExercise/src/components/Demo2.vue
+++ cssExercise/src/components/Demo2.vue
@@ -1,5 +1,4 @@
 <template>
-  <header class="flex justify-center bg-slate-200 m-4 font-bold text-3xl p-4 rounded-lg absolute top">CSS3D Experience</header>
   <div class="my_container" ref="container">
     <div class="title" :style="rotate3DObject">Feed your eyes, feed your soul.</div>
     <div class="sub_title" :style="rotate3DObject">开眼，看更好的世界。</div>
@@ -7,7 +6,7 @@
 </template>
 
 <script setup>
-import { onMounted, reactive, ref, watch, watchEffect } from 'vue'
+import { ref, watch } from 'vue'
 import { useMouse } from '@vueuse/core'
 
 const { x, y, sourceType } = useMouse()
@@ -20,14 +19,14 @@ const width = window.innerWidth
 const height = window.innerHeight
 
 watch(x, () => {
-  deltaRotateY.value = (width / 2 - x.value) * 0.1
+  deltaRotateY.value = (x.value - width / 2 ) * 0.08
   rotate3DObject.value = {
     transform: `rotateX(${deltaRotateX.value}deg) rotateY(${deltaRotateY.value}deg)`
   }
 })
 
 watch(y, () => {
-  deltaRotateX.value = (y.value - height / 2) * 0.1
+  deltaRotateX.value = (height / 2 - y.value ) * 0.08
   rotate3DObject.value = {
     transform: `rotateX(${deltaRotateX.value}deg) rotateY(${deltaRotateY.value}deg)`
   }
@@ -42,6 +41,7 @@ watch(y, () => {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  perspective: 800px;
 }
 .title {
   font-size: 70px;
