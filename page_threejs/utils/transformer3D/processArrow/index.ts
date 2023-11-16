1713d81edbf1d68b198f9bc04f866e30e96b6139
丁学武 <dingxw@qq.com>
2023-11-16 16:06:01 +0800

    fix: 删除tsf3D

--- page_threejs/utils/transformer3D/processArrow/index.ts
+++
@@ -1,4 +0,0 @@
-export { process_ShowTransformArrow } from './process_ShowTransformArrow'
-export { process_ShowRotateArrow } from './process_ShowRotateArrow'
-export { process_ShowRotateRing } from './process_ShowRotateRing'
-export { process_ShowRotateHelp } from './process_ShowRotateHelper'
